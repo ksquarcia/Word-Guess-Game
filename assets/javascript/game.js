aa99979f98fc676b7778691320ff4428686f7c66
Kyle Squarcia <ksquarcia2008@gmail.com>
2019-02-21 23:39:37 -0800

    pictures

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -65,23 +65,23 @@ function checkArrays(arr1, arr2) {
 // function to display animal photo after a win
 function displayPhoto(medieval) {
     if (medieval == "longbow") {
-        medievalImage.src = "#"
+        medievalImage.src = "assets/images/longbow.jpg"
     } else if (medieval == "dagger") {
-        medievalImage.src = "#"
+        medievalImage.src = "assets/images/dagger.jpg"
     } else if (medieval == "swords") {
-        medievalImage.src = "#"
+        medievalImage.src = "assets/images/swords.jpg"
     } else if (medieval == "musket") {
-        medievalImage.src = "#"
+        medievalImage.src = "assets/images/musket.jpg"
     } else if (medieval == "elephant") {
-        medievalImage.src = "#"
+        medievalImage.src = "assets/images/elephant.jpg"
     } else if (medieval == "pike") {
-        medievalImage.src = "#"
+        medievalImage.src = "assets/images/pike.jpg"
     } else if (medieval == "castle") {
-        medievalImage.src = "#"
+        medievalImage.src = "assets/images/castle.jpg"
     } else if (medieval == "moat") {
-        medievalImage.src = "#"
+        medievalImage.src = "assets/images/moat.jpg"
     } else if (medieval == "drawbridge") {
-        medievalImage.src = "#"
+        medievalImage.src = "assets/images/drawbridge.jpg"
     }
 }
 
